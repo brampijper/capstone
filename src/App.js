0b84eb0dac9101a5be6565d69380c8557378aa55
Bram <brampijper@gmail.com>
2023-02-01 16:49:11 +0100

    implemented basic navigation with react router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
 import { Header } from './components/Header';
 import { Cart } from "./pages/Cart";
@@ -8,6 +10,11 @@ function App () {
     <div className="App">
       <Header />
       <h1>Capstone Project</h1>
+
+      <Routes>
+        <Route path="/" element={ <Photos /> } />
+        <Route path="/cart" element={ <Cart /> } />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'remixicon/fonts/remixicon.css'
 import './index.css';
 import App from './App';
@@ -8,9 +9,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
